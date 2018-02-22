574674158c0f9825d6788ce3d4140f4171f499b3
Anthony DURIF <anthony.durif@uca.fr>
2018-02-22 16:48:32 +0100

    Init version

---
+++ version.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2018020801;
+$plugin->requires  = 2017051500;
+$plugin->cron      = 0;
+$plugin->component = 'local_uca_transfer_courses';
+$plugin->release   = 'v3.3-r1';
