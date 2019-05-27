bfb728b874b093b0c7c453a599f086c118166adc
Anthony DURIF <anthony.durif@uca.fr>
2019-05-27 11:17:45 +0200

    Provider Update

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -27,6 +27,9 @@ namespace local_uca_transfer_courses\privacy;
 
 use \core_privacy\local\metadata\collection;
 use core_privacy\local\request\approved_contextlist;
+use core_privacy\local\request\approved_userlist;
+use core_privacy\local\request\contextlist;
+use core_privacy\local\request\data_provider;
 
 defined('MOODLE_INTERNAL') || die();
 
@@ -36,8 +39,9 @@ defined('MOODLE_INTERNAL') || die();
  * @copyright  2019 Université Clermont Auvergne
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-//class provider implements \core_privacy\local\metadata\provider {
-class provider implements \core_privacy\local\metadata\null_provider {
+class provider implements \core_privacy\local\metadata\provider, data_provider
+{
+//class provider implements \core_privacy\local\metadata\null_provider {
 
     /**
      * Get the language string identifier with the component's language
@@ -45,7 +49,8 @@ class provider implements \core_privacy\local\metadata\null_provider {
      *
      * @return  string
      */
-    public static function get_reason() : string {
+    public static function get_reason(): string
+    {
         return 'privacy:metadata';
     }
 
@@ -55,7 +60,8 @@ class provider implements \core_privacy\local\metadata\null_provider {
      *
      * @return  string
      */
-    public static function get_metadata(collection $items) : collection {
+    public static function get_metadata(collection $items): collection
+    {
         $items->add_database_table(
             'course_transfer_request',
             [
@@ -68,11 +74,43 @@ class provider implements \core_privacy\local\metadata\null_provider {
     }
 
     /**
+     * Get the list of contexts related to users who made a transfer request.
+     *
+     * @param int $userid The user to search.
+     *
+     * @return contextlist $contextlist The contextlist containing the list of contexts used in this plugin.
+     */
+    public static function get_contexts_for_userid($userid)
+    {
+        $contextlist = new contextlist();
+
+        // Context for content_user_data.
+        $cudsql = "
+          SELECT
+            c.id
+          FROM {context} c
+            INNER JOIN {user} u ON u.id = c.instanceid
+          WHERE c.contextlevel = :contextlevel
+                AND d.user_id = :userid
+        ";
+
+        $cudparams = array(
+            'contextlevel' => CONTEXT_USER,
+            'userid' => $userid,
+        );
+
+        $contextlist->add_from_sql($cudsql, $cudparams);
+
+        return $contextlist;
+    }
+
+    /**
      * Export all user data for the specified user, in the specified contexts.
      *
      * @param approved_contextlist $contextlist The approved contexts to export information for.
      */
-    public static function export_user_data(approved_contextlist $contextlist) {
+    public static function export_user_data(approved_contextlist $contextlist)
+    {
         if (empty($contextlist)) {
             return;
         }
@@ -88,7 +126,7 @@ class provider implements \core_privacy\local\metadata\null_provider {
                         AND cfr.user_id = :user";
                 $params = [
                     'level' => CONTEXT_USER,
-                    'user'  => $contextlist->get_user()->id
+                    'user' => $contextlist->get_user()->id
                 ];
 
                 $requests = $DB->get_records_sql($sql, $params);
@@ -110,7 +148,8 @@ class provider implements \core_privacy\local\metadata\null_provider {
      *
      * @param approved_contextlist $contextlist The approved contexts and user information to delete information for.
      */
-    public static function delete_data_for_user(approved_contextlist $contextlist) {
+    public static function delete_data_for_user(approved_contextlist $contextlist)
+    {
         global $DB;
         if (empty($contextlist->count())) {
             return;
@@ -118,4 +157,48 @@ class provider implements \core_privacy\local\metadata\null_provider {
         $userid = $contextlist->get_user()->id;
         $DB->delete_records('course_transfer_request', array('user_id' => $userid));
     }
+
+    /**
+     * Delete all personal data for all users in the specified context.
+     *
+     * @param context $context Context to delete data from.
+     */
+    public static function delete_data_for_all_users_in_context(\context $context)
+    {
+        global $DB;
+        if ($context->contextlevel != CONTEXT_USER) {
+            return;
+        }
+        $userid = $context->instanceid;
+        $DB->delete_records('course_transfer_request', array('user_id' => $userid));
+    }
+
+    /**
+     * Get the list of users who have data within a context.
+     *
+     * @param userlist $userlist The userlist containing the list of users who have data in this context/plugin combination.
+     */
+    public static function get_users_in_context(userlist $userlist) {
+        $context = $userlist->get_context();
+
+        if (!$context instanceof \context_user) {
+            return;
+        }
+
+        $sql = "SELECT d.userid FROM {course_transfer_request} ctr";
+        $userlist->add_from_sql('userid', $sql);
+    }
+
+    /**
+     * Delete multiple users within a single context.
+     *
+     * @param approved_userlist $userlist The approved context and user information to delete information for.
+     */
+    public static function delete_data_for_users(approved_userlist $userlist) {
+        global $DB;
+
+        $users = $userlist->get_userids();
+        $DB->delete_records_select('course_transfer_request', 'user_id IN :usersid', ['users' => $users]);
+    }
 }
+
